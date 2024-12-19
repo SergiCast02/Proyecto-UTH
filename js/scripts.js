fbc320314f46ea78aa7b6eb2d3e2ef1895ed853f
Wilfredo Espinal <wilfredo.espinal@outlook.com>
2024-12-19 15:06:46 -0600

    agregados todos los productos y correcciones fuente responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 $(document).ready(function(){
   $('.carousel').slick({
+    slidesToShow: 2,
     infinite: true,
     dots: true,
     speed: 700,
     arrows: true,
+    adaptiveHeight: true,
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 431,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
   });
 
   // Cambiar la imagen de fondo del contenedor del carrusel al cambiar el producto visible
@@ -14,4 +34,8 @@ $(document).ready(function(){
     var newBackground = currentSlideElement.data('bg');
     $('.carousel-background').css('background-image', 'url(images/' + newBackground + ')');
   });
+
+  // Establecer la imagen de fondo inicial al cargar la página
+  var initialBackground = $('.carousel .carousel-item:first').data('bg');
+  $('.carousel-background').css('background-image', 'url(images/' + initialBackground + ')');
 });
